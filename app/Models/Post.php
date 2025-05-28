4bad6e9a76a193e8cc87b5fa0f9a0362f0734b4a
Az-31 <azaanul10@gmail.com>
2025-05-28 22:18:45 +0700

    migration database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,46 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Judul 1',
-                'author' => 'Author 1',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam earum ex harum numquam, iure laboriosam unde repellendus doloremque f
-            acere reiciendis incidunt cumque distinctio iusto qui excepturi minima autem ut ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aut odio laboriosam ratione ea vel totam at fugiat magnam nesciunt, quisquam sint repellendus atque praesentium, dolores quaerat adipisci natus autem?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Judul 2',
-                'author' => 'Author 2',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, totam facilis. Eum, saepe nulla. Aliquam minima explicabo culpa quidem sed aperiam atque esse suscipit id, cupiditate inventore velit magnam cum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos aut odio laboriosam ratione ea vel totam at fugiat magnam nesciunt, quisquam sint repellendus atque praesentium, dolores quaerat adipisci natus autem?'
-            ]
-
-        ];
-    }
-
-    public static function find($id)
-    {
-        // callback
-        //     return Arr::first(static::all(), function($post) use ($id){
-        //     return $post['id'] == $id;
-        // });
-
-        // arrow funcion
-        // return Arr::first(static::all(), fn($post) => $post['id'] == $id);
-        
-        $post = Arr::first(static::all(), fn($post) => $post['id'] == $id);
-
-         if(!$post){
-            abort(404);
-         }
-
-         return $post;
-    }
+   
 }
